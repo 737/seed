8b22eac02fcef7c9dcc662df6630b94446521073
liwl李文利 <liwl@Ctrip.com>
2016-09-09 17:56:24 +0800

    update redux connect 方法

--- react-redux/02-counter/containers/App.js
+++ react-redux/02-counter/containers/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Counter from '../components/Counter'
 import * as actions from '../actions/counter'
 
 console.log(actions);
 
-export default connect(state => {
-    return {
-        counter: state.counter
-    }
-}, actions)(Counter)
+//将state.counter绑定到props的counter
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  }
+}
+//将action的所有方法绑定到props上
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
